fbab1dfe638ab8a89205c6883e8a926c21074831
Galaxy <wanghaochen56@gmail.com>
2018-11-29 20:34:18 +0800

    添加LiveDataEventBus

--- app/src/main/java/com/galaxydl/e_university/data/source/network/OkHttpClientManager.kt
+++ app/src/main/java/com/galaxydl/e_university/data/source/network/OkHttpClientManager.kt
@@ -1,11 +1,12 @@
 package com.galaxydl.e_university.data.source.network
 
+import android.support.v4.util.ArrayMap
 import com.galaxydl.e_university.data.source.network.cookie.CookieManager
 import okhttp3.OkHttpClient
 
 object OkHttpClientManager {
 
-    private val clients = HashMap<String, OkHttpClient>()
+    private val clients = ArrayMap<String, OkHttpClient>()
 
     fun getClient(key: String): OkHttpClient {
         var client = clients[key]
---
+++ app/src/main/java/com/galaxydl/e_university/utils/LiveDataEventBus.kt
@@ -0,0 +1,53 @@
+package com.galaxydl.e_university.utils
+
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
+import android.util.ArrayMap
+
+object LiveDataEventBus {
+
+    private val buses: Map<String, BusLiveData<Any>>
+            by lazy { ArrayMap<String, BusLiveData<Any>>() }
+
+    fun get(key: String): MutableLiveData<Any> = buses[key]!!
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T> get(key: String, clazz: Class<T>): MutableLiveData<T> = get(key) as MutableLiveData<T>
+
+    private class BusLiveData<T>(private val mEventKey: String) : MutableLiveData<T>() {
+
+        var needNotify = false
+            private set(v) {
+                field = v
+            }
+
+        override fun setValue(value: T) {
+            needNotify = true
+            super.setValue(value)
+        }
+
+        override fun postValue(value: T) {
+            needNotify = true
+            super.postValue(value)
+        }
+
+        override fun observe(owner: LifecycleOwner, observer: Observer<T>) {
+            super.observe(owner, BusObserver(observer, this))
+        }
+
+    }
+
+    private class BusObserver<T>(private val mObserver: Observer<T>,
+                                 private val mLiveData: BusLiveData<T>) : Observer<T> {
+
+        override fun onChanged(t: T?) {
+            if (mLiveData.needNotify) {
+                if (t != null) {
+                    mObserver.onChanged(t)
+                }
+            }
+        }
+
+    }
+}
